c3be16e24792bce787a8a74b4b4ab8db548fb7eb
dheisongomes <gomesdheison24@gmail.com>
2026-06-22 21:54:18 -0300

    Desafio loja concluido

---
+++ Desafio04/loja.js
@@ -0,0 +1,25 @@
+/*
+
+DESAFIO 04 - LOJA TEWCH
+Uma loja de informática possui vários produtos cadastrados.
+
+Crie um array de objetos contendo:
+- nome
+- preço
+
+Utilize uma estrutura de repetição para mostrar apenas os produtos que custam mais de R$100,00.
+Exiba o nome dos produtos encontrados.
+*/
+
+let produtos = [
+    {nome: "Teclado", preco: 159.90},
+    {nome: "Mouse Gamer", preco: 249.90},
+    {nome: "Fone de Ouvido", preco: 89.90},
+];
+
+for(let i = 0; i < produtos.length; i++) {
+    if(produtos[i].preco > 100){
+        console.log(produtos[i].nome);
+    }
+}
+    
