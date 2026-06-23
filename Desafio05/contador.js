8160fb2aa45c82c1bbfe674cfca6553be14166d1
dheisongomes <gomesdheison24@gmail.com>
2026-06-22 21:53:28 -0300

    Desafio contador concluido

---
+++ Desafio05/contador.js
@@ -0,0 +1,25 @@
+/* DESAFIO 05 - CONTADOR INTELIGENTE
+
+Crie uma função chamada contador () que receba:
+- valor inicial
+- valor final
+
+A função deve exibir todos os números entre os valores informados.
+
+Após concluir , crie uma segunda versão capaz de realizar
+a contagem regressiva.
+*/
+
+function contador(inicial, final) {
+    for(let i = inicial; i <= final; i++){
+        console.log(i);
+    }
+}
+
+function contadorRegressivo(inicial, final) {
+    for(let i = inicial; i >= final; i--){
+        console.log(i);
+    }
+}
+contador(0, 10)
+contadorRegressivo(10, 0)
